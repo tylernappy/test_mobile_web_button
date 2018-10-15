62e96e1b20d701162d07e081620a30021883fdc8
Tyler Nappy <tyler@usebutton.com>
2018-10-15 11:22:19 -0400

    Added URL query string to autofill the link

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 // index.js
-var path = require('path')
+var path = require('path');
 
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-var port = process.env.PORT || 5000
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+var port = process.env.PORT || 5000;
 
 app.get('/:html_file', function(req, res) {
-  res.sendFile(path.join(__dirname, 'views', req.params.html_file + '.html'))
-})
+  res.sendFile(path.join(__dirname, 'views', req.params.html_file + '.html'));
+});
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'))
-})
+  const url = req.query.url;
+  res.render('index', {
+    url,
+  });
+});
 
 app.listen(port, function() {
-  console.log('Listening on port: ' + port)
-})
+  console.log('Listening on port: ' + port);
+});
